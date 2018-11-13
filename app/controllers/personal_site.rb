f7618b27a6c92f60794598f159187015d4b720ac
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-13 16:54:29 -0700

    Adds passing test with updated personal_site class.

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,7 +2,7 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-type' => 'text/html'}, ['Welcome!']]
+    ['200', {'Content-type' => 'text/html'}, [File.read('./app/views/index.html')]]
     #this array includes the HTTP response status code, HTTP response headers & HTTP body
   end
 end
