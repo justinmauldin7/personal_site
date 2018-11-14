4fe13fac63d49b71cfd1c7a1e0b11a5459218abd
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-13 17:24:02 -0700

    Adds abbout to personal_site class with refactor with helper method.

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -4,17 +4,26 @@ class PersonalSite
   def self.call(env)
     case env["PATH_INFO"]
     when '/' then index
+    when '/about' then about
     else
       error
     end
   end
 
   def self.index
-    ['200', {'Content-type' => 'text/html'}, [File.read('./app/views/index.html')]]
-    #this array includes the HTTP response status code, HTTP response headers & HTTP body
+    render_view('index.html')
+  end
+
+  def self.about
+    render_view('about.html')
   end
 
   def self.error
-    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
+    render_view('error.html')
+  end
+
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
+    #this array includes the HTTP response status code, HTTP response headers & HTTP body
   end
 end
