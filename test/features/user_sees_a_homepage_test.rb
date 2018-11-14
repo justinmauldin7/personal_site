6acce529d9bd906863f21d32452fd792f18cb70e
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-13 17:16:16 -0700

    Adds test for About page.

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -13,4 +13,11 @@ class HomepageTest < CapybaraTestCase
 
     assert_equal 404, page.status_code
   end
+
+  def test_user_can_see_the_about_page
+    visit '/about'
+
+    assert page.has_content?("About Me!")
+    assert_equal 200, page.status_code
+  end
 end
