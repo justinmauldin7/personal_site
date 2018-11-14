36dca8baa9de6a25cd2dfd22be1f67666365d998
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-13 17:13:13 -0700

    Adds changes to Personal_Site class to account for error to unknown link.

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,7 +2,19 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
+    case env["PATH_INFO"]
+    when '/' then index
+    else
+      error
+    end
+  end
+
+  def self.index
     ['200', {'Content-type' => 'text/html'}, [File.read('./app/views/index.html')]]
     #this array includes the HTTP response status code, HTTP response headers & HTTP body
+  end
+
+  def self.error
+    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
   end
 end
